b45fa3db808a7ce97c836870b980b00c46a87e26
Matthias Lampert <perrhh.comworld@gmail.com>
2024-12-18 09:28:32 +0100

    new: basic template

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+// defining the Template engine
+// templates are files inside `./views` and its subdirectories
+// that end with `.go.html`
+var engine = html.New("./views", ".go.html")
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello Gophers!")
+		t_data := fiber.Map{
+			"Name": "Jackie",
+			"Host": "Fiber Meetups",
+		}
+		return c.Render("index", t_data)
 	})
 
 	app.Listen(":3000")
