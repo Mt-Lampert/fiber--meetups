101c03626f788cf88006b35f42fa98d6089e503b
Matthias Lampert <perrhh.comworld@gmail.com>
2024-12-20 10:52:27 +0100

    new: Meetup type and template ranges

--- handlers.go
+++ handlers.go
@@ -2,10 +2,24 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+type Meetup struct {
+	Title    string
+	Location string
+	Date     string
+}
+
 func index(c *fiber.Ctx) error {
-	t_data := fiber.Map{
-		"Name": "Matthew",
-		"Host": "Meetups Inc.",
+	Meetups := []Meetup{
+		{
+			Title:    "Fiesta Floridiana",
+			Location: "Fort Lauderdale, FA",
+			Date:     "2025-06-13",
+		},
+		{
+			Title:    "Vinyard Reaping",
+			Location: "Martha's Vinyard, MA",
+			Date:     "2025-04-09",
+		},
 	}
-	return c.Render("index", t_data)
+	return c.Render("index", Meetups)
 }
