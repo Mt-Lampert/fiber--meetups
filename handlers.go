4684cf6059c636dc2597eafd718e404b5f503f1b
Matthias Lampert <perrhh.comworld@gmail.com>
2024-12-20 21:00:26 +0100

    new: meetup images; pp: formatting meetup instances

--- handlers.go
+++ handlers.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 type Meetup struct {
 	Title    string
 	Location string
 	Date     string
+	Image    string
+	ImgAlt   string
 }
 
 func index(c *fiber.Ctx) error {
@@ -14,11 +18,15 @@ func index(c *fiber.Ctx) error {
 			Title:    "Fiesta Floridiana",
 			Location: "Fort Lauderdale, FA",
 			Date:     "2025-06-13",
+			Image:    "florida-keys.png",
+			ImgAlt:   "Florida Keys img",
 		},
 		{
 			Title:    "Vinyard Reaping",
 			Location: "Martha's Vinyard, MA",
 			Date:     "2025-04-09",
+			Image:    "The-Vinyard-SPA.png",
+			ImgAlt:   "The Vineyard img",
 		},
 	}
 	return c.Render("index", Meetups)
